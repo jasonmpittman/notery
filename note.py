c440d38fa59c93725c587f1583c900014fead41c
Jason M. Pittman <jason@jasonmpittman>
2022-02-01 04:52:31 -0500

    refactored methods to notery.py

--- note.py
+++ note.py
@@ -10,12 +10,6 @@ __email__ = "jason@jasonmpittman.com"
 __status__ = "Development"
 
 
-import time
-import os
-import configparser
-from datetime import datetime
-
-
 class Note:
 
     __header = {
@@ -26,37 +20,9 @@ class Note:
     }
 
     def __init__(self, n):
-        self.__header['title'] = n.Title # i think we need to parse the short sentence from n.Text
-        self.__header['body'] = n.Text
-        self.__header['id'] = n.Title #the title in gkeep is the date based id
-        self.__header['tags'] = n.Labels
+        self.__header['title'] = n.title # i think we need to parse the short sentence from n.Text
+        self.__header['body'] = n.text
+        self.__header['id'] = n.title #the title in gkeep is the date based id
+        self.__header['tags'] = n.labels
 
-    def parse_text(self):
-        #need to parse gkeep 'text' into note components
-        #regex for hashtags
-        #regex for id datestring
-
-        pass
-
-    
-
-    def write_note(self):
-        now = datetime.now()
-        stamp = now.strftime("%Y%d%m%H%M")
-
-        try:
-            f = open(stamp + ".md", "w+")
-            f.write('---\n')
-            
-            for k, v in self.__header.items():
-                f.write(str(k) + ': ' + str(v) + '\n')
-            
-            f.write('---\n\n')
-
-            fields = self.__body.split(',')
-            for field in fields:
-                f.write('## ' + field.strip(' ') + '\n\n')
-
-            f.close()
-        except Exception as e:
-            print('Error writing note: ' + str(e))
+        #print(self.__header)
