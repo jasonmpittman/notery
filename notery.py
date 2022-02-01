0d9d84cb1786eab5136aa13f5f83bbc0fcf3d100
Jason M. Pittman <jason@jasonmpittman>
2022-02-01 04:53:11 -0500

    updated methods to handle notes

--- notery.py
+++ notery.py
@@ -11,6 +11,11 @@ __status__ = "Development"
 
 import json
 import gkeepapi
+import time
+import os
+from datetime import datetime
+
+from note import Note
 
 class Notery:
 
@@ -25,22 +30,30 @@ class Notery:
 
     def load_tags(self):
         with open('tags.json', 'r', encoding='utf-8') as f:
-            tags = json.load(f)
+            t = json.load(f)
+        
+        tags = t['Tags']
         
         return tags
 
     def get_notes(self, keep):
-        
+        gnotes = []    
+        notes = []
 
-        tags = self.load_tags()
-        for tag in tags['Tags']:
-            print(tag)
+        t = self.load_tags()
+        tags = [x.strip() for x in t[0].split(',')]
 
-        notes = keep.find(labels=[keep.findLabel('reference')])
+        for tag in tags:
+
+            gnotes = keep.find(labels=[keep.findLabel(tag)])
+            
+            for gnote in gnotes:
+                n = Note(gnote)
+                notes.append(n)
+                #print([note.title, note.text])
+                #print("\n")            
         
-        for note in notes:
-            print([note.title, note.text])
-            print("\n")
+        return notes
 
     def login(self):
         keep = gkeepapi.Keep()
@@ -52,15 +65,43 @@ class Notery:
         finally:
             return keep
 
-    def write_zettel(self, note):
+
+    def parse_text(self):
+        #need to parse gkeep 'text' into note components
+        #regex for hashtags
+        #regex for id datestring
+
         pass
+
+    def write_zettel(self, note):
+        now = datetime.now()
+        stamp = now.strftime("%Y%d%m%H%M")
+
+        try:
+            f = open(stamp + ".md", "w+")
+            f.write('---\n')
+            
+            for k, v in self.__header.items():
+                f.write(str(k) + ': ' + str(v) + '\n')
+            
+            f.write('---\n\n')
+
+            fields = self.__body.split(',')
+            for field in fields:
+                f.write('## ' + field.strip(' ') + '\n\n')
+
+            f.close()
+        except Exception as e:
+            print('Error writing note: ' + str(e))
 
 
 def main():
     notery = Notery()
     keep = notery.login()
     
-    notery.get_notes(keep) #(keep.all())
+    notes = notery.get_notes(keep) #(keep.all())
+
+    
 
 if __name__ == "__main__":
     main()
